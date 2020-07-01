79208e031d44545238522bfb0bd3c2adc5f9f6e6
venturemackital <44316926+AlexMackenzieWS@users.noreply.github.com>
2020-07-02 00:25:32 +0100

    Add files via upload

---
+++ build/precache-manifest.cfffcadbfb8c48b2ce929ae12ace127d.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ba07fcb1ff99ec48c171d97d62fa9497",
+    "url": "/index.html"
+  },
+  {
+    "revision": "41751fde734328ba9caf",
+    "url": "/static/css/main.bffad5ce.chunk.css"
+  },
+  {
+    "revision": "f6ea9f7f3b80a1590008",
+    "url": "/static/js/2.9ea4762e.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.9ea4762e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "41751fde734328ba9caf",
+    "url": "/static/js/main.2caac04c.chunk.js"
+  },
+  {
+    "revision": "2235f34fabf5c4eb3c2a",
+    "url": "/static/js/runtime-main.485bf12e.js"
+  },
+  {
+    "revision": "26f7872c1f4fc7d8e7411fb2a69bfbca",
+    "url": "/static/media/AlexImagePN.26f7872c.png"
+  },
+  {
+    "revision": "69c1ea5ccd3b858a08b583ced6c5ccf0",
+    "url": "/static/media/Gatsby .69c1ea5c.png"
+  },
+  {
+    "revision": "8c00598757ea750382f4a211331f8ae0",
+    "url": "/static/media/GraphQL.8c005987.png"
+  },
+  {
+    "revision": "580ffd26ccaa16481ed87c1829c74943",
+    "url": "/static/media/React.580ffd26.png"
+  },
+  {
+    "revision": "83fb4e935ec43deec7bdadc5d3620cbf",
+    "url": "/static/media/React_logo.83fb4e93.png"
+  },
+  {
+    "revision": "0dfae3280e388283dc05ffc8620bf1e5",
+    "url": "/static/media/Tensorflow.0dfae328.png"
+  },
+  {
+    "revision": "fa84fbeed0e650d947208a32437e4fc4",
+    "url": "/static/media/apple-logo.fa84fbee.png"
+  },
+  {
+    "revision": "70fa86de5562edd99cac83ad5bbb9090",
+    "url": "/static/media/auth0.70fa86de.png"
+  },
+  {
+    "revision": "04badecd2799ba76b599d038533ca32f",
+    "url": "/static/media/bootstrap.04badecd.png"
+  },
+  {
+    "revision": "23a7dee5995afbb2f9ad3b32799c6f82",
+    "url": "/static/media/gmail.23a7dee5.svg"
+  },
+  {
+    "revision": "ecc6f9e5d804ae0db7490c683af2f406",
+    "url": "/static/media/mongodb.ecc6f9e5.png"
+  },
+  {
+    "revision": "ebd58ae022cfe61e351cfae9adc8f2be",
+    "url": "/static/media/pandas.ebd58ae0.png"
+  },
+  {
+    "revision": "a162f1ee10d257abc6325e58fbb2a51a",
+    "url": "/static/media/spotify-logo-png.a162f1ee.png"
+  },
+  {
+    "revision": "b7edbdb578884125badc1104ee079e63",
+    "url": "/static/media/stripe.b7edbdb5.png"
+  },
+  {
+    "revision": "3f449766b8973964778b5a7fdc2c178c",
+    "url": "/static/media/twitter-logo.3f449766.png"
+  },
+  {
+    "revision": "2d8eb5e342327bf974b8d519780eb0b2",
+    "url": "/static/media/twitter.2d8eb5e3.svg"
+  },
+  {
+    "revision": "c500a3801d8356a86da86a06c3d13a4d",
+    "url": "/static/media/webpack.c500a380.png"
+  }
+]);
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.adf7c5d06f9b3d127347df2497562d0e.js"
+  "/precache-manifest.cfffcadbfb8c48b2ce929ae12ace127d.js"
 );
 
 self.addEventListener('message', (event) => {
